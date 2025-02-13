771ef48ae753d36fa57684ed71d87bad6b2c44bd
Jason Fu <r.fu1@student.tue.nl>
2025-02-13 22:36:00 +0000

    eslint change

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,7 @@ import tseslint from "typescript-eslint";
 export default tseslint.config(
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, tseslint.configs.recommended, pluginQuery.configs.recommended],
+    extends: [js.configs.recommended, tseslint.configs.recommended, pluginQuery.configs["flat/recommended"]],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,

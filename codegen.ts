859656d819bdce0f1ceff7504d260b266b15f799
Jason Fu <95495174+fasonju@users.noreply.github.com>
2025-02-14 00:19:13 +0100

    Graphql setup
    * graphql setup
    
    * apollo client
    
    * switch to react-query
    
    * whitelines
    
    * eslint change

---
+++ codegen.ts
@@ -0,0 +1,14 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+ 
+const config: CodegenConfig = {
+   schema: "schema.graphql",
+   documents: ["src/**/*.tsx", "src/**/*.graphql"],
+   generates: {
+      "./__generated__/": {
+         preset: "client",
+      },
+      
+   },
+};
+
+export default config;

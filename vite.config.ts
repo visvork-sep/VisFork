283502fff0447d653c2e4356d480baf2d1dac80e
Quinnca77 <50358541+Quinnca77@users.noreply.github.com>
2025-02-14 18:53:45 +0000

    Silence warnings related to third-party code

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+        scss: {
+            api: "modern-compiler", 
+            // SASS deprecated some things that bootstrap hasnt patched yet (even in the latest release!)
+            // so this line is only meant to keep the terminal clean of warnings related to third-party code
+            silenceDeprecations: ["mixed-decls", "color-functions", "global-builtin", "import"]
+        }
+    }
+},
 });

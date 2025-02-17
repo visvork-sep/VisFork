862be17323d2a423695de23a1f7e896e7f2c3d11
Jason Fu <r.fu1@student.tue.nl>
2025-02-17 14:27:52 +0000

    setup usequery

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,31 @@
 import { BaseStyles, Box, ThemeProvider } from "@primer/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { paths } from "@generated/rest-schema.js";
 import App from "./App.tsx";
 import "./index.css";
+import createClient from "openapi-fetch";
+
+
+/**
+ * Queryclient for React-query calls
+ */
+const queryClient = new QueryClient();
+
+const fetchClient = createClient<paths>({
+  baseUrl: process.env.API_URL // TODO! add the correct URL
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider colorMode="auto">
       <BaseStyles>
-        <Box className="body">
-          <App/>
-        </Box>
+        <QueryClientProvider client={queryClient}>
+          <Box className="body">
+            <App/>
+          </Box>
+        </QueryClientProvider>
       </BaseStyles>
     </ThemeProvider>
   </StrictMode>

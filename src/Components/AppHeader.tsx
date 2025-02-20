00355da8884779ee408d41fa7045ee9b53a74e40
Jason Fu <r.fu1@student.tue.nl>
2025-02-20 14:19:37 +0000

    light/dark mode toggle

--- src/Components/AppHeader.tsx
+++ src/Components/AppHeader.tsx
@@ -1,30 +1,33 @@
 import { ImageIcon, MoonIcon, SunIcon } from "@primer/octicons-react";
-import { Avatar, Box, Button, Dialog, Header } from "@primer/react";
+import { Avatar, Box, Button, Dialog, Header, useTheme } from "@primer/react";
 import { ActionList } from "@primer/react/deprecated";
 import { useCallback, useState } from "react";
 
 
 function AppHeader() {
+    const {colorMode, setColorMode} = useTheme();
     const [isOpen, setIsOpen] = useState(false);
     const [loggedIn, setLoggedIn] = useState(false); // TODO: Implement login
     const [colorblindMode, setColorblindMode] = useState(false); // todo implement colorblind mode
-    const [darkMode, setDarkMode] = useState(false); // todo implement dark mode
+
 
     const onDialogClose = useCallback(() => setIsOpen(false), []);
     const onDialogOpen = useCallback(() => setIsOpen(true), []);
     const onLogin = useCallback(() => setLoggedIn(true), []);
-    const onToggleColorblindMode = useCallback(() => {
-        setColorblindMode((colorblindMode) => !colorblindMode)
-    }, [colorblindMode]);
     const onLogout = useCallback(() => {
         onDialogClose();
         setLoggedIn(false);
     }, []);
-    const ontoggleDarkMode = useCallback(() => {
-        setDarkMode((darkMode) => !darkMode)
-    }, []);
 
+    const onToggleColorblindMode = useCallback(() => {
+        setColorblindMode((colorblindMode) => !colorblindMode)
+    }, [colorblindMode]);
 
+    const currentlyDarkMode = colorMode === "dark" || colorMode === "night";
+    const onToggleDarkMode = useCallback(() => {
+        const newMode = currentlyDarkMode ? "day" : "dark";
+        setColorMode(newMode);
+    }, [currentlyDarkMode]);
 
     return (
         <>
@@ -56,8 +59,8 @@ function AppHeader() {
                         items={[
                             {text: "colorblind mode", onClick: onToggleColorblindMode, selected: colorblindMode},
 
-                            {text: darkMode ? "light mode" :"dark mode", onClick: ontoggleDarkMode,
-                                leadingVisual: darkMode ? SunIcon : MoonIcon},
+                            {text: currentlyDarkMode ? "light mode" :"dark mode", onClick: onToggleDarkMode,
+                                leadingVisual: currentlyDarkMode ? SunIcon : MoonIcon},
                             ActionList.Divider,
                             {text: "Sign out", onClick: onLogout, variant: "danger"},
                         ]}>
--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider colorMode="auto">
+    <ThemeProvider colorMode="day">
       <BaseStyles>
         <Box className="body">
           <App/>

515912f05f90552afbaa25f8fd7666900da012bd
Jason Fu <r.fu1@student.tue.nl>
2025-02-23 17:57:47 +0000

    jsx component not as function

--- src/Components/ConfigurationPane/ConfigurationPane.tsx
+++ src/Components/ConfigurationPane/ConfigurationPane.tsx
@@ -4,20 +4,18 @@ import { Pagehead } from "@primer/react/deprecated";
 import FilterForm from "@Components/ConfigurationPane/FilterForm";
 import FileButtons from "@Components/ConfigurationPane/FileButtons";
 
-const fileButtons = FileButtons();
+
 
 function ConfigurationPane() {
-    const filterForm = FilterForm();
-
     return(
         <Stack gap="condensed">
             <Stack.Item>
-                {filterForm}
+                <FilterForm/> 
             </Stack.Item> 
 
             <Pagehead>Import/Export</Pagehead>
             <Stack.Item>
-                {fileButtons}
+                <FileButtons/>
             </Stack.Item>             
         </Stack>
     );

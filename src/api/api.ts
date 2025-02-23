6be05c18e33b4a0dac7ccbd81cf4ed89144a86a9
SUDO-Gabor <g.viniczay@student.tue.nl>
2025-02-23 13:16:15 +0100

    Added configuration to use the environment variables.

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,17 +1,19 @@
+import {BACKEND_URL} from "../Components/Configuration/Configuration";
+
 export async function fetchUser() {
-    const response = await fetch(`${process.env.BACKEND_URL}/auth/user`, {
+    const response = await fetch(`${BACKEND_URL}/auth/user`, {
         credentials: "include",
     });
     return response.ok ? response.json() : null;
 }
 
 export async function logoutUser() {
-    return fetch(`${process.env.BACKEND_URL}/auth/logout`, {
+    return fetch(`${BACKEND_URL}/auth/logout`, {
         method: "POST",
         credentials: "include",
     });
 }
 
 export async function loginUser() {
-    window.location.href = `${process.env.BACKEND_URL}/auth/github`;
+    window.location.href = `${BACKEND_URL}/auth/github`;
 }

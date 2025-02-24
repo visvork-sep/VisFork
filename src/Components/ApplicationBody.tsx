cf3f54cf10884f57b84b60bc8a00eef6827af416
alva-shudofsky <alva.shudofsky@gmail.com>
2025-02-24 17:53:39 +0000

    Rough commit table implementation.

--- src/Components/ApplicationBody.tsx
+++ src/Components/ApplicationBody.tsx
@@ -1,24 +1,44 @@
-import {   Spinner, Stack } from "@primer/react";
+// import {   Spinner, Stack } from "@primer/react";
 
-import { Blankslate } from "@primer/react/experimental";
-
-const plotsData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// import { Blankslate } from "@primer/react/experimental";
+import { Stack } from '@primer/react';
+import CommitTable from './Plots/CommitTable';
+import { Blankslate } from '@primer/react/experimental';
+// import { Stack, Spinner } from "@primer/react";
+// import { Blankslate } from "@primer/react/experimental";
 
 function ApplicationBody() {
-    const children = plotsData.map((plot) => {
-        return (
-            <Stack.Item key={plot}>
-                <Blankslate border>
-                    <Blankslate.Heading>Plot{plot}</Blankslate.Heading>
-                    <Blankslate.Visual>
-                        <Spinner/>
-                    </Blankslate.Visual>
-                </Blankslate>
+
+    return (
+        <>
+            <Stack.Item>
+                <Blankslate.Visual>
+                    <CommitTable/>
+                </Blankslate.Visual>
             </Stack.Item>
-        );
-    });
-  
-    return children;
+        </>
+        
+
+    )
 }
+
+// const plotsData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// function ApplicationBody() {
+//     const children = plotsData.map((plot) => {
+//         return (
+//             <Stack.Item key={plot}>
+//                 <Blankslate border>
+//                     <Blankslate.Heading>Hi{plot}</Blankslate.Heading>
+//                     <Blankslate.Visual>
+//                         <Spinner/>
+//                     </Blankslate.Visual>
+//                 </Blankslate>
+//             </Stack.Item>
+//         );
+//     });
+  
+//     return children;
+// }
 
 export default ApplicationBody;
---
+++ src/Components/Plots/CommitTable.tsx
@@ -0,0 +1,132 @@
+import {DataTable, Table} from '@primer/react/experimental';
+import { Box, Link, TextInput, useTheme } from '@primer/react';
+import commitData from './commit_data_example.json';
+import { useState } from 'react';
+
+interface CommitInfo {
+  repo: string;
+  sha: string;
+  id: string;
+  parentIds: string[];
+  branch_name: string;
+  branch_id: string;
+  node_id: string;
+  author: string;
+  date: string;
+  url: string;
+  message: string;
+  commit_type: string;
+  mergedNodes: unknown[];
+}
+
+function CommitTable() {
+
+    const {colorMode} = useTheme();
+
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const data: CommitInfo[] = commitData;
+
+    const isDarkMode = colorMode === 'dark' || colorMode === 'night';
+    const linkColor = isDarkMode ? 'white' : 'black';
+
+    const filteredData = data.filter((commit) =>
+        commit.message.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    return (
+        <Box>
+            <Box mb={3}>
+                <TextInput 
+                    placeholder="Search by commit message..." 
+                    value={searchTerm} 
+                    onChange={(e) => setSearchTerm(e.target.value)} 
+                    aria-label="Search commit messages"
+                    style={{width: '400px'}}
+                />
+            </Box>
+
+        
+            <Table.Container>
+                <DataTable
+                    data={filteredData}
+                    columns={[
+                        {
+                            header: 'Owner/Repo',
+                            field: 'repo',
+                            rowHeader: true,
+                            width: 'auto',
+                            renderCell: row => (
+                                <Link
+                                    href={`https://github.com/${row.repo}`} 
+                                    target="_blank" 
+                                    rel="noopener noreferrer"
+                                    style={{
+                                        color: linkColor,
+                                        textDecoration: 'none',
+                                    }}
+                                >
+                                    {row.repo}
+                                </Link>
+                            )
+                        },
+                        {
+                            header: 'Author',
+                            field: 'author',
+                            rowHeader: true,
+                            width: 'auto',
+                            // Doesn't work perfectly rn, since author's name can differ from the username, 
+                            // and url is based on username which we don't have
+                            renderCell: row => (
+                                <Link
+                                    href={`https://github.com/${row.author}`} 
+                                    target="_blank" 
+                                    rel="noopener noreferrer"
+                                    style={{
+                                        color: linkColor,
+                                        textDecoration: 'none',
+                                    }}
+                                >
+                                    {row.author}
+                                </Link>
+                            )
+                        },
+                        {
+                            header: 'Commit Date',
+                            field: 'date',
+                            rowHeader: true,
+                            width: 'auto',
+                        },
+                        {
+                            header: 'Commit Message',
+                            field: 'message',
+                            rowHeader: true,
+                            width: 'auto',
+                        },
+                        {
+                            header: 'URL',
+                            field: 'url',
+                            rowHeader: true,
+                            width: 'auto',
+                            renderCell: row => (
+                                <Link
+                                    href={row.url}
+                                    target="_blank" 
+                                    rel="noopener noreferrer"
+                                    style={{
+                                        color: linkColor,
+                                        textDecoration: 'none',
+                                    }}
+                                >
+                                    {row.url}
+                                </Link>
+                            )
+                        },
+                    ]}
+                />
+            </Table.Container>
+        </Box>
+    );
+};
+
+export default CommitTable;

fdfc9b16e3324e266d6935901a9040140102e242
Jason Fu <r.fu1@student.tue.nl>
2025-02-24 15:05:57 +0000

    constants whitespace

--- src/Utils/Constants.ts
+++ src/Utils/Constants.ts
@@ -2,8 +2,6 @@
 export const MIN_FORKS = 1;
 /// For numforks input: Maximum amount of forks to query for
 export const MAX_FORKS = 200;
-
-
 /// For recent activity filter: Lower bound for recenct activity filtering
 export const MOST_RECENT_UPDATE = 1;
 /// For recent activity filter: Upper bound for recenct activity filtering

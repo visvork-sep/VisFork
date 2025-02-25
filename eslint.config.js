79d22ada9f65232c106a2f2c01280f4e78a088fb
aleks-chitalov <alex55bg@gmail.com>
2025-02-25 12:55:08 +0100

    Squash merge develop into basic-queries

--- eslint.config.js
+++ eslint.config.js
@@ -10,9 +10,15 @@ export default [
     {languageOptions: { globals: globals.browser }},
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
+    ...tseslint.configs.strict,
+    ...tseslint.configs.stylistic,
     pluginReact.configs.flat.recommended,
     {
         rules: {
+            "quotes": ["error", "double", {}],
+            "linebreak-style": ["error", "windows"],
+            "no-irregular-whitespace": ["error"],
+            "semi": ["error", "always"],
             "max-len": ["error", { code: 120, tabWidth: 4 }],
             "indent": ["error", 4, { SwitchCase: 1 }],
             "react/jsx-indent": ["error", 4],

68941dbbc459e0ff14bd58e684470053ab3972b9
Greg <vesz.gergo@gmail.com>
2025-02-25 19:58:43 +0100

    Added all fork filters and extracted existing ones to separate types (DateRange, OwnerType)

--- src/Types/Filters.ts
+++ src/Types/Filters.ts
@@ -1,17 +1,24 @@
-// Define possible sorting options
-export type SortingCriterion = "stargazers" | "watchers" | "oldest" | "newest";
+export type DateRange = {
+    start?: string;
+    end?: string
+};
 
-// Define possible fork types
+export type SortingCriterion = "stargazers" | "watchers" | "oldest" | "newest" | "latestCommit" | "authorPopularity";
+
 export type ForkType = "adaptive" | "corrective" | "perfective";
 
+export type OwnerType = "user" | "organization";
+
+
 // Define the filter state structure
+// See URD Section 3.1.3. "Filtering and Sorting Options"
 export interface ForkFilters {
-    dateRange: { start?: string; end?: string };
-    sortBy: SortingCriterion;
+    dateRange: DateRange;
+    sortBy: SortingCriterion; // Need to set default value to "stargazers" for URF-048
     activeForksOnly?: boolean;
     forkType?: ForkType;
-    sortByLastCommit?: boolean;
-    ownerType?: "user" | "organization";
+    // sortByLastCommit?: boolean; -> moved to SortingCriterion "latestCommit"
+    ownerType?: OwnerType;
     updatedInLastMonths?: number;
 }
 

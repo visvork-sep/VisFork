91f26e2701512332456b305cfdf37ef911d8ddaf
Jason Fu <r.fu1@student.tue.nl>
2025-02-26 21:18:36 +0000

    minimal good looking summarry

--- src/Components/ApplicationBody.tsx
+++ src/Components/ApplicationBody.tsx
@@ -1,24 +1,46 @@
-import {   Spinner, Stack } from "@primer/react";
+import {   Box, Details, Spinner, Stack } from "@primer/react";
 
-import { Blankslate } from "@primer/react/experimental";
+import ForkList from "./Plots/ForkList";
 
 const plotsData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function ApplicationBody() {
+
     const children = plotsData.map((plot) => {
         return (
             <Stack.Item key={plot}>
-                <Blankslate border>
-                    <Blankslate.Heading>Plot{plot}</Blankslate.Heading>
-                    <Blankslate.Visual>
-                        <Spinner/>
-                    </Blankslate.Visual>
-                </Blankslate>
+                <Details sx={{
+                    "> summary::-webkit-details-marker": {
+                        display: "revert"
+                    },
+                    "> summary": {
+                        backgroundColor: "neutral.muted",
+                        padding: "5px"
+                    }
+                }}>
+                    <Details.Summary>Fork list</Details.Summary>
+                    <ForkList forks={[
+                        {
+                            id: 0,
+                            full_name: "name",
+                            description: "description"
+                        },
+                        {
+                            id: 1,
+                            full_name: "name2",
+                            description: "description 2"
+                        }
+                    ]}/>
+                </Details>
             </Stack.Item>
         );
     });
   
-    return children;
+    return(
+    <Stack>
+        {children}
+    </Stack>
+    );
 }
 
 export default ApplicationBody;

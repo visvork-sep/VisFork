73230421c7c9b8f6e539fce676b46e5f0e1b72b1
Kaloyan <105170643+Kala-Konarev@users.noreply.github.com>
2025-02-26 11:28:34 +0100

    Implement first version of ForkList. (#24)
    * Implement first version of ForkList. Add hot reload option for faster development.
    
    * Remove dummy data comment.

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
+    server: {
+        watch: {
+            usePolling: true
+        }
+    }
 });

b41f7ccc27270aa7cde278d8718ae95231c73687
alva-shudofsky <alva.shudofsky@gmail.com>
2025-02-26 14:32:34 +0000

    Defined columns above return statement.

--- src/Components/Plots/CommitTable.tsx
+++ src/Components/Plots/CommitTable.tsx
@@ -1,4 +1,4 @@
-import {DataTable, Table} from "@primer/react/experimental";
+import {Column, DataTable, Table} from "@primer/react/experimental";
 import { Box, Link, TextInput, useTheme } from "@primer/react";
 import commitData from "./commit_data_example.json";
 import { useState } from "react";
@@ -39,6 +39,89 @@ function CommitTable() {
     const isDarkMode = colorMode === "dark" || colorMode === "night";
     const linkColor = isDarkMode ? "white" : "black";
 
+    // Define how each column will be displayed
+    const columns: Column<CommitInfo>[] = [
+        {
+            header: "Owner/Repo",
+            field: "repo",
+            rowHeader: true,
+            width: "auto",
+            renderCell: (row: CommitInfo) => (
+                <Link
+                    href={`https://github.com/${row.repo}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    sx={{
+                        color: linkColor,
+                        textDecoration: "none",
+                    }}
+                >
+                    {row.repo}
+                </Link>
+            ),
+        },
+        {
+            header: "Author",
+            field: "author",
+            rowHeader: true,
+            width: "auto",
+            // Note: Doesn't work perfectly if author's name doesn't match GitHub username
+            renderCell: (row: CommitInfo) => (
+                <Link
+                    href={`https://github.com/${row.author}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    sx={{
+                        color: linkColor,
+                        textDecoration: "none",
+                    }}
+                >
+                    {row.author}
+                </Link>
+            ),
+        },
+        {
+            header: "Commit Date",
+            field: "date",
+            rowHeader: true,
+            width: "auto",
+        },
+        {
+            header: "Commit Message",
+            field: "message",
+            rowHeader: true,
+            width: "auto",
+        },
+        {
+            header: "URL",
+            field: "url",
+            rowHeader: true,
+            width: "auto",
+            renderCell: (row: CommitInfo) => (
+                <Box
+                    sx={{
+                        whiteSpace: "normal",
+                        wordBreak: "break-all",
+                        overflowWrap: "break-word",
+                        minWidth: "100px",
+                    }}
+                >
+                    <Link
+                        href={row.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        sx={{
+                            color: linkColor,
+                            textDecoration: "none",
+                        }}
+                    >
+                        {row.url}
+                    </Link>
+                </Box>
+            ),
+        },
+    ];
+
     return (
         <Box>
             {/* Container for search input */}
@@ -63,88 +146,7 @@ function CommitTable() {
                         // Table will be populated with filtered data
                         data={filteredData}
 
-                        // Define how each column will be displayed
-                        columns={[
-                            {
-                                header: "Owner/Repo",
-                                field: "repo",
-                                rowHeader: true,
-                                width: "auto",
-                                renderCell: row => (
-                                    <Link
-                                        href={"https://github.com/${row.repo}"} 
-                                        target="_blank" 
-                                        rel="noopener noreferrer"
-                                        sx={{
-                                            color: linkColor,
-                                            textDecoration: "none",
-                                        }}
-                                    >
-                                        {row.repo}
-                                    </Link>
-                                )
-                            },
-                            {
-                                header: "Author",
-                                field: "author",
-                                rowHeader: true,
-                                width: "auto",
-                                // Note: Doesn't work perfectly rn, since author's name can differ from the username, 
-                                // and url is based on username which we don't have
-                                renderCell: row => (
-                                    <Link
-                                        href={"https://github.com/${row.author}"} 
-                                        target="_blank" 
-                                        rel="noopener noreferrer"
-                                        sx={{
-                                            color: linkColor,
-                                            textDecoration: "none",
-                                        }}
-                                    >
-                                        {row.author}
-                                    </Link>
-                                )
-                            },
-                            {
-                                header: "Commit Date",
-                                field: "date",
-                                rowHeader: true,
-                                width: "auto",
-                            },
-                            {
-                                header: "Commit Message",
-                                field: "message",
-                                rowHeader: true,
-                                width: "auto",
-                            },
-                            {
-                                header: "URL",
-                                field: "url",
-                                rowHeader: true,
-                                width: "auto" ,
-                                renderCell: row => (
-                                    <Box
-                                        sx={{
-                                            whiteSpace: "normal",
-                                            wordBreak: "break-all",
-                                            overflowWrap: "break-word",
-                                            minWidth: "100px"
-                                        }}
-                                    >
-                                        <Link
-                                            href={row.url}
-                                            target="_blank"
-                                            rel="noopener noreferrer"
-                                            sx={{
-                                                color: linkColor,
-                                                textDecoration: "none",
-                                            }}
-                                        >
-                                            {row.url}
-                                        </Link>
-                                    </Box>
-                                )},
-                        ]}
+                        columns={columns}
                     />
                 </Table.Container>
             </Box>

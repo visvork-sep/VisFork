6f9bcc9713a19010c8d3d80d39311d83bae44f05
Jason Fu <r.fu1@student.tue.nl>
2025-02-26 21:35:23 +0000

    working dropdown

--- src/Components/ApplicationBody.tsx
+++ src/Components/ApplicationBody.tsx
@@ -1,6 +1,7 @@
 import {   Box, Details, Spinner, Stack } from "@primer/react";
 
-import ForkList from "./Plots/ForkList";
+import ForkList from "@Components/Plots/ForkList";
+import { Dropdown } from "@Components/Dropdown";
 
 const plotsData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
@@ -9,29 +10,20 @@ function ApplicationBody() {
     const children = plotsData.map((plot) => {
         return (
             <Stack.Item key={plot}>
-                <Details sx={{
-                    "> summary::-webkit-details-marker": {
-                        display: "revert"
-                    },
-                    "> summary": {
-                        backgroundColor: "neutral.muted",
-                        padding: "5px"
-                    }
-                }}>
-                    <Details.Summary>Fork list</Details.Summary>
+                <Dropdown summaryText="Fork List">
                     <ForkList forks={[
-                        {
-                            id: 0,
-                            full_name: "name",
-                            description: "description"
-                        },
-                        {
-                            id: 1,
-                            full_name: "name2",
-                            description: "description 2"
-                        }
-                    ]}/>
-                </Details>
+                            {
+                                id: 0,
+                                full_name: "name",
+                                description: "description"
+                            },
+                            {
+                                id: 1,
+                                full_name: "name2",
+                                description: "description 2"
+                            }
+                        ]}/>
+                </Dropdown>
             </Stack.Item>
         );
     });

2919fd03787af750e2f228b4e2e3084ebbdea176
Jason Fu <r.fu1@student.tue.nl>
2025-02-27 10:55:16 +0000

    fix header bug

--- src/Components/AppHeader.tsx
+++ src/Components/AppHeader.tsx
@@ -1,10 +1,10 @@
 import { ImageIcon, MoonIcon, SunIcon } from "@primer/octicons-react";
-import { ActionMenu, Avatar, Box, Button, Dialog, Header, useTheme } from "@primer/react";
+import { ActionMenu, Avatar, Box, Button, Dialog, Stack, useTheme } from "@primer/react";
 import { ActionList } from "@primer/react/deprecated";
 import { useCallback, useState } from "react";
 
 function AppHeader() {
-    const {colorMode, setColorMode, dayScheme, nightScheme, setDayScheme, setNightScheme} = useTheme();
+    const { colorMode, setColorMode, dayScheme, nightScheme, setDayScheme, setNightScheme } = useTheme();
     const [isOpen, setIsOpen] = useState(false);
     const [loggedIn, setLoggedIn] = useState(false); // TODO: Implement login
 
@@ -34,46 +34,75 @@ function AppHeader() {
 
     return (
         <>
-            <Header aria-label="nav bar">
-                <Header.Item>
-                    <ImageIcon size={32}/>
-                </Header.Item>
-                <Header.Item full>
+            {/* <Header aria-label="nav bar" sx={{
+                backgroundColor: "canvas.subtle",
+                color: "black"
+            }}> */}
+            <Stack direction="horizontal" align="center">
+                <Stack.Item>
+                    <ImageIcon size={32} />
+                </Stack.Item>
+                <Stack.Item grow>
                     <span>VisFork</span>
-                </Header.Item>
-
-                <Header.Item aria-label="Open settings dialog">
+                </Stack.Item>
+                <Stack.Item>
                     {loggedIn ? (
-                        <Box onClick={onDialogOpen}> 
-                            <Avatar src="https://avatars.githubusercontent.com/u/14032476?v=4" size={24}/> 
+                        <Box onClick={onDialogOpen}>
+                            <Avatar src="https://avatars.githubusercontent.com/u/14032476?v=4" size={24} />
                         </Box>
-                        
-                    ) : ( 
+                    ) : (
                         <Button onClick={onLogin} aria-label="Login button">
                             Log in
                         </Button>
                     )}
-                </Header.Item>
-            </Header>
+
+                </Stack.Item>
+            </Stack>
+            {/* <Header.Item>
+                <ImageIcon size={32} />
+            </Header.Item>
+            <Header.Item full>
+                <span>VisFork</span>
+            </Header.Item>
+
+            <Header.Item aria-label="Open settings dialog">
+                {loggedIn ? (
+                    <Box onClick={onDialogOpen}>
+                        <Avatar src="https://avatars.githubusercontent.com/u/14032476?v=4" size={24} />
+                    </Box>
+
+                ) : (
+                    <Button onClick={onLogin} aria-label="Login button">
+                        Log in
+                    </Button>
+                )}
+            </Header.Item> */}
+            {/* </Header> */}
 
             {isOpen && (
                 <Dialog title="Settings" onClose={onDialogClose} position={"right"} width="small">
                     <ActionMenu>
-                        <ActionList 
+                        <ActionList
                             items={[
-                                {text: "Colorblind mode", onClick: onToggleColorblindMode, 
-                                    selected: currentlyColorblindMode, "aria-label": "Toggle colrblind mode"},
-                                {text: currentlyDarkMode ? "Light mode" :"Dark mode", onClick: onToggleDarkMode,
+                                {
+                                    text: "Colorblind mode", onClick: onToggleColorblindMode,
+                                    selected: currentlyColorblindMode, "aria-label": "Toggle colrblind mode"
+                                },
+                                {
+                                    text: currentlyDarkMode ? "Light mode" : "Dark mode", onClick: onToggleDarkMode,
                                     leadingVisual: currentlyDarkMode ? SunIcon : MoonIcon,
-                                    "aria-label": "Toggle light mode"},
+                                    "aria-label": "Toggle light mode"
+                                },
                                 ActionList.Divider,
-                                {text: "Sign out", onClick: onLogout, variant: "danger",
-                                    "aria-label": "Sign out button"},
+                                {
+                                    text: "Sign out", onClick: onLogout, variant: "danger",
+                                    "aria-label": "Sign out button"
+                                },
                             ]}>
                         </ActionList>
                     </ActionMenu>
-                    
-                    
+
+
                 </Dialog>
             )}
         </>

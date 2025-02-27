11334aaf85b8010331e15e99bd4d57ba61f759f8
Kala-Konarev <kkonarev144@gmail.com>
2025-02-27 19:50:00 +0000

    Finish merge changes.

--- src/Components/ApplicationBody.tsx
+++ src/Components/ApplicationBody.tsx
@@ -1,35 +1,13 @@
 import { Stack } from "@primer/react";
-
-import ForkList from "@Components/Plots/ForkList";
-import { Dropdown } from "@Components/Dropdown";
-
-const plotsData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+import exampleData from "../Components/Plots/commit_data_example.json";
+import facebookData from "../Components/Plots/facebook_react-commit_data.json";
+import Barchart from "./Plots/Barchart";
 
 function ApplicationBody() {
-    const children = plotsData.map((plot) => {
-        return (
-            <Stack.Item key={plot}>
-                <Dropdown summaryText="Fork List">
-                    <ForkList forks={[
-                        {
-                            id: 0,
-                            full_name: "name",
-                            description: "description"
-                        },
-                        {
-                            id: 1,
-                            full_name: "name2",
-                            description: "description 2"
-                        }
-                    ]} />
-                </Dropdown>
-            </Stack.Item>
-        );
-    });
-
     return (
         <Stack>
-            {children}
+            <Barchart rawData={facebookData} />
+            <Barchart rawData={exampleData} />
         </Stack>
     );
 }
--- src/Components/Plots/Plot1.tsx
+++ src/Components/Plots/Plot1.tsx
@@ -1,49 +1,49 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import * as d3 from "d3";
 
 function Plot1() {
-  const svgRef = useRef<SVGSVGElement | null>(null); // Explicit type for TypeScript
+    const svgRef = useRef<SVGSVGElement | null>(null); // Explicit type for TypeScript
 
-  useEffect(() => {
-    if (!svgRef.current) return;
+    useEffect(() => {
+        if (!svgRef.current) return;
 
-    const data = [10, 20, 30, 40, 50];
+        const data = [10, 20, 30, 40, 50];
 
-    const width = 300;
-    const height = 150;
+        const width = 300;
+        const height = 150;
 
-    const svg = d3
-      .select(svgRef.current)
-      .attr("width", width)
-      .attr("height", height);
+        const svg = d3
+            .select(svgRef.current)
+            .attr("width", width)
+            .attr("height", height);
 
-    const xScale = d3
-      .scaleBand<number>()
-      .domain(data.map((_, i) => i))
-      .range([0, width])
-      .padding(0.2);
+        const xScale = d3
+            .scaleBand<number>()
+            .domain(data.map((_, i) => i))
+            .range([0, width])
+            .padding(0.2);
 
-    const yScale = d3
-      .scaleLinear()
-      .domain([0, d3.max(data) as number])
-      .range([height, 0]);
+        const yScale = d3
+            .scaleLinear()
+            .domain([0, d3.max(data) as number])
+            .range([height, 0]);
 
-    svg
-      .selectAll("rect")
-      .data(data)
-      .join("rect")
-      .attr("x", (_, i) => xScale(i)!)
-      .attr("y", (d) => yScale(d)!)
-      .attr("width", xScale.bandwidth())
-      .attr("height", (d) => height - yScale(d)!)
-      .attr("fill", "steelblue");
-  }, []);
+        svg
+            .selectAll("rect")
+            .data(data)
+            .join("rect")
+            .attr("x", (_, i) => xScale(i))
+            .attr("y", (d) => yScale(d)!)
+            .attr("width", xScale.bandwidth())
+            .attr("height", (d) => height - yScale(d)!)
+            .attr("fill", "steelblue");
+    }, []);
 
-  return (
-    <div>
-      <svg ref={svgRef}></svg>
-    </div>
-  );
+    return (
+        <div>
+            <svg ref={svgRef}></svg>
+        </div>
+    );
 }
 
 export default Plot1;

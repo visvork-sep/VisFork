22c98e4ff367bd2c749dab241885c65f2062b061
Jason Fu <r.fu1@student.tue.nl>
2025-03-01 00:19:17 +0000

    remove unused import

--- src/Utils/Auth.ts
+++ src/Utils/Auth.ts
@@ -1,11 +1,4 @@
-import createClient from "openapi-fetch";
-import { paths } from "@generated/auth-schema";
-import { useQuery } from "@tanstack/react-query";
 import { AUTH_URL } from "@Utils/Constants";
-
-const fetchClient = createClient<paths>({
-    baseUrl: AUTH_URL
-});
 
 function redirectLogin() {
     setTimeout(() => { // timeout for safari behavior
@@ -13,19 +6,6 @@ function redirectLogin() {
     }, 250);
 }
 
-function useExchangeAccessToken(parameters: paths["/auth/github/token"]["get"]["parameters"]) {
-    return useQuery({
-        queryKey: ["Login"],
-        queryFn: async () => {
-            return fetchClient.GET("/auth/github/token", {
-                params: parameters
-            });
-        },
-        gcTime: 0
-    });
-}
-
 export {
     redirectLogin,
-    useExchangeAccessToken
 };

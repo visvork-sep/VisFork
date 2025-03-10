9febda23a2ba1a1128480dbf13ca5bd9c6368a4d
Jason Fu <r.fu1@student.tue.nl>
2025-03-10 23:08:26 +0000

    dry for error text

--- src/Types/FormErrors.ts
+++ src/Types/FormErrors.ts
@@ -1,36 +1,55 @@
 import { MAX_QUERIABLE_FORKS, MIN_QUERIABLE_FORKS, RECENT_ACTIVITY_MAX_MONTHS, RECENT_ACTIVITY_MIN_MONTHS }
     from "@Utils/Constants";
 
+const unknownError = { message: "Unknown error" };
+
+const laterFromDateError =
+    { message: "The date range is not valid, make sure the from date is before the until date." };
+
+const futureUntilDateError = { message: "Future dates are not allowed for the until field" };
+
+const lessThanMinForksError = { message: `Number of forks must be greater than ${MIN_QUERIABLE_FORKS}` };
+
+const greaterThanMaxForksError = { message: `Number of forks must be less than ${MAX_QUERIABLE_FORKS}` };
+
+const repositorySyntaxError = { message: "Syntax must match <Owner>/<RepositoryName>" };
+
+const ownerError = { message: "Invalid repository owner" };
+
+const repositoryNameError = { message: "Invalid repository name" };
+
+const outOfRecentlyUpdatedRangeError = {
+    message: `Can only filter on recent updates between 
+            ${RECENT_ACTIVITY_MIN_MONTHS} ago and ${RECENT_ACTIVITY_MAX_MONTHS} ago`
+};
+
 const CommitsDateRangeFromInputErrors = {
-    LaterFromDateError: { message: "The date range is not valid, make sure the from date is before the until date." },
-    UnknownError: { message: "Unknown error" },
+    LaterFromDateError: laterFromDateError,
+    UnknownError: unknownError
 } as const;
 
 const CommitsDateRangeUntilInputErrors = {
-    LaterFromDateError: { message: "The date range is not valid, make sure the from date is before the until date." },
-    UnknownError: { message: "Unknown error" },
-    FutureUntilDateError: { message: "Future dates are not allowed for the until field" },
+    LaterFromDateError: laterFromDateError,
+    UnknownError: unknownError,
+    FutureUntilDateError: futureUntilDateError
 } as const;
 
 const ForksCountInputErrors = {
-    LessThanMinForksError: { message: `Number of forks must be greater than ${MIN_QUERIABLE_FORKS}` },
-    GreaterThanMaxForksError: { message: `Number of forks must be less than ${MAX_QUERIABLE_FORKS}` },
-    UnknownError: { message: "Unknown error" },
+    LessThanMinForksError: lessThanMinForksError,
+    GreaterThanMaxForksError: greaterThanMaxForksError,
+    UnknownError: unknownError
 } as const;
 
 const RepositoryInputErrors = {
-    SyntaxError: { message: "Syntax must match <Owner>/<RepositoryName>" },
-    OwnerError: { message: "Invalid repository owner" },
-    RepositoryNameError: { message: "Invalid repository name" },
-    UnknownError: { message: "Unknown error" },
+    RepositorySyntaxError: repositorySyntaxError,
+    OwnerError: ownerError,
+    RepositoryNameError: repositoryNameError,
+    UnknownError: unknownError
 } as const;
 
 const RecentlyUpdatedInputErrors = {
-    OutOfInputRangeError: {
-        message: `Can only filter on recent updates between 
-            ${RECENT_ACTIVITY_MIN_MONTHS} ago and ${RECENT_ACTIVITY_MAX_MONTHS} ago`
-    },
-    UnknownError: { message: "Unknown error" },
+    OutOfRecentlyUpdatedRangeError: outOfRecentlyUpdatedRangeError,
+    UnknownError: unknownError
 } as const;
 
 // Derive strict types from error objects

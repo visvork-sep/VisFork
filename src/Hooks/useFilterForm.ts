740f963358f57fefe632994d836466d4e4a2c76f
Jason Fu <r.fu1@student.tue.nl>
2025-03-13 19:55:08 +0000

    improve comments

--- src/Hooks/useFilterForm.ts
+++ src/Hooks/useFilterForm.ts
@@ -1,24 +1,30 @@
 import { useState, useCallback } from "react";
 import { FilterFormState } from "../Types/FilterForm";
-import { FORK_TYPES, FORKS_COUNT_INPUT_INITIAL, FORKS_SORTING_ORDERS, OWNER_TYPES, SORT_DIRECTION }
+import { FORK_TYPES, FORKS_COUNT_INPUT_INITIAL, FORKS_SORTING_ORDERS, OWNER_TYPES, SORT_DIRECTION } 
     from "@Utils/Constants";
 import { assert } from "@Utils/Assert";
 
+// Initial state for the filter form
 const initialForm: FilterFormState = {
-    repository: "",
-    forksCount: FORKS_COUNT_INPUT_INITIAL,
-    forksOrder: FORKS_SORTING_ORDERS.STARGAZERS.value,
-    forksAscDesc: SORT_DIRECTION.ASCENDING.value,
-    forksTypeFilter: Object.values(FORK_TYPES).map(t => t.value),
-    ownerTypeFilter: Object.values(OWNER_TYPES).map(t => t.value),
-    commitsDateRangeFrom: "",
-    commitsDateRangeUntil: "",
-    recentlyUpdated: ""
+    repository: "", // Repository name input
+    forksCount: FORKS_COUNT_INPUT_INITIAL, // Default fork count input
+    forksOrder: FORKS_SORTING_ORDERS.STARGAZERS.value, // Default sorting order (by stargazers)
+    forksAscDesc: SORT_DIRECTION.ASCENDING.value, // Default sorting direction (ascending)
+    forksTypeFilter: Object.values(FORK_TYPES).map(t => t.value), // Default fork type filter (all types selected)
+    ownerTypeFilter: Object.values(OWNER_TYPES).map(t => t.value), // Default owner type filter (all types selected)
+    commitsDateRangeFrom: "", // Start date for commits filter
+    commitsDateRangeUntil: "", // End date for commits filter
+    recentlyUpdated: "" // Recently updated filter input
 };
 
 function useFilterForm() {
     const [form, setForm] = useState<FilterFormState>(initialForm);
 
+    /**
+     * Updates the repository input field in the form state.
+     * 
+     * @param input - The new repository name entered by the user.
+     */
     const handleRepositoryChange = useCallback((input: string) => {
         setForm((prev) => ({
             ...prev,
@@ -27,78 +33,91 @@ function useFilterForm() {
     }, []);
 
     /**
-     * Modifies the state of forksCount
-     * Note that the count will be set to undefined if the input is not a number.
+     * Updates the forks count filter in the form state.
+     * 
+     * @param input - The new fork count input as a string.
+     *                If the input is not a valid number, the state will be set to undefined.
      */
     const handleForksCountChange = useCallback((input: string) => {
         setForm((prev) => ({ ...prev, forksCount: input }));
     }, []);
 
-
     /**
-     * Changes the state for forksOrder,
+     * Updates the forks sorting order.
      * 
-     * @precondition value \in Object.values(FORKS_SORTING_ORDERS).map(t => t.value)
+     * @param value - The selected sorting order.
+     * @precondition The value must be one of the defined sorting options in FORKS_SORTING_ORDERS.
      */
     const handleForksOrderChange = useCallback((value: string) => {
-        assert(Object.values(FORKS_SORTING_ORDERS).map(t => t.value).includes(value), "Precondition broken");
+        assert(Object.values(FORKS_SORTING_ORDERS).map(t => t.value).includes(value), "Invalid sorting order selected");
 
         setForm((prev) => ({ ...prev, forksOrder: value }));
     }, []);
 
     /**
-     * Changes the state for forksOrderAscDesc
+     * Updates the sorting direction (ascending/descending) for forks.
      * 
-     * @precondition value \in Object.values(SORT_DIRECTION).map(t => t.value) 
+     * @param value - The selected sorting direction.
+     * @precondition The value must be one of the defined sorting directions in SORT_DIRECTION.
      */
     const handleForksOrderAscDescChange = useCallback((value: string) => {
-        assert(Object.values(SORT_DIRECTION).map(t => t.value).includes(value), "Precondition broken");
+        assert(Object.values(SORT_DIRECTION).map(t => t.value).includes(value), "Invalid sorting direction selected");
 
         setForm((prev) => ({ ...prev, forksAscDesc: value }));
     }, []);
 
     /**
-     * Changes the state of commitsDateRangeFrom
+     * Updates the start date for commit filtering.
+     * 
+     * @param input - The new start date entered by the user.
      */
     const handleCommitsDateRangeFromChange = useCallback((input: string) => {
         setForm((prev) => ({ ...prev, commitsDateRangeFrom: input }));
     }, []);
 
     /**
-     * Changes the state of commitsDateRangeUntil
+     * Updates the end date for commit filtering.
+     * 
+     * @param input - The new end date entered by the user.
      */
     const handleCommitsDateRangeUntilChange = useCallback((input: string) => {
         setForm((prev) => ({ ...prev, commitsDateRangeUntil: input }));
     }, []);
 
     /**
-     * Changes the state of forksTypeFilter
+     * Updates the selected fork types for filtering.
      * 
-     * @precondition (\all : string s \in selected : s \in FORK_TYPES)
+     * @param selected - An array of selected fork type values.
+     * @precondition All selected values must be present in the predefined FORK_TYPES.
      */
     const handleForksTypeFilterChange = useCallback((selected: string[]) => {
-        assert(selected.every(s => Object.values(FORK_TYPES).map(t => t.value).includes(s)));
+        assert(selected.every(s => Object.values(FORK_TYPES).map(t => t.value).includes(s)),
+            "Invalid fork type selected");
 
         setForm((prev) => ({ ...prev, forksTypeFilter: selected }));
     }, []);
 
     /**
-     * Changes the state of onwerTypeFilter
+     * Updates the selected owner types for filtering.
      * 
-     * @precondition (\all : string s \in selected : s \in OWNER_TYPES)
+     * @param selected - An array of selected owner type values.
+     * @precondition All selected values must be present in the predefined OWNER_TYPES.
      */
     const handleOwnerTypeFilterChange = useCallback((selected: string[]) => {
-        assert(selected.every(s => Object.values(OWNER_TYPES).map(t => t.value).includes(s)));
+        assert(selected.every(s => Object.values(OWNER_TYPES).map(t => t.value).includes(s)),
+            "Invalid owner type selected");
 
         setForm((prev) => ({ ...prev, ownerTypeFilter: selected }));
     }, []);
 
     /**
-     * Modifies the state of recentlyUpdated
-     * Note that the count will be set to undefined if the input is not a number.
+     * Updates the recently updated filter input.
+     * 
+     * @param input - The new value for the recently updated filter.
+     *                If the input is not a valid number, the state will be set to undefined.
      */
     const handleRecentlyUpdatedChange = useCallback((input: string) => {
-        setForm((prev) => ({ ...prev, recentlyUpdated: input}));
+        setForm((prev) => ({ ...prev, recentlyUpdated: input }));
     }, []);
 
     return {
@@ -113,7 +132,7 @@ function useFilterForm() {
         handleOwnerTypeFilterChange,
         handleRecentlyUpdatedChange,
     };
-};
+}
 
 export {
     useFilterForm
--- src/Utils/Assert.ts
+++ src/Utils/Assert.ts
@@ -4,12 +4,20 @@ class AssertionError extends Error {
         this.name = "AssertionError";
     }
 }
+
 function assertFn(condition: boolean, message?: string) {
     if (!condition) {
         throw new AssertionError(message);
     }
 }
 
+/**
+ * Assert function that only is included in the development build,
+ * will get stripped out for production builds
+ * 
+ * @param condition Assertion condition
+ * @param message Error message
+ */
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 export const assert = import.meta.env.DEV ? assertFn : () => {};
 export {

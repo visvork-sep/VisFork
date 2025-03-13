ebb21c27d04c9e02bc3970639a6eb2094ca746fd
Jason Fu <r.fu1@student.tue.nl>
2025-03-13 19:56:10 +0000

    improved comments

--- src/Types/FormErrors.ts
+++ src/Types/FormErrors.ts
@@ -1,45 +1,102 @@
-import { MAX_QUERIABLE_FORKS, MIN_QUERIABLE_FORKS, RECENT_ACTIVITY_MAX_MONTHS, RECENT_ACTIVITY_MIN_MONTHS }
-    from "@Utils/Constants";
+import {
+    MAX_QUERIABLE_FORKS,
+    MIN_QUERIABLE_FORKS,
+    RECENT_ACTIVITY_MAX_MONTHS,
+    RECENT_ACTIVITY_MIN_MONTHS
+} from "@Utils/Constants";
 
+/**
+ * Default error message for unknown issues.
+ */
 const unknownError = { message: "Unknown error" };
 
-const laterFromDateError =
-    { message: "The date range is not valid, make sure the from date is before the until date." };
-
-const futureUntilDateError = { message: "Future dates are not allowed for the until field" };
-
-const lessThanMinForksError = { message: `Number of forks must be greater than ${MIN_QUERIABLE_FORKS}` };
-
-const greaterThanMaxForksError = { message: `Number of forks must be less than ${MAX_QUERIABLE_FORKS}` };
-
-const repositorySyntaxError = { message: "Syntax must match <Owner>/<RepositoryName>" };
-
-const ownerError = { message: "Invalid repository owner" };
-
-const repositoryNameError = { message: "Invalid repository name" };
-
-const outOfRecentlyUpdatedRangeError = {
-    message: `Can only filter on recent updates between 
-            ${RECENT_ACTIVITY_MIN_MONTHS} ago and ${RECENT_ACTIVITY_MAX_MONTHS} ago`
+/**
+ * Error message for invalid date ranges where the "from" date is after the "until" date.
+ */
+const laterFromDateError = {
+    message: "The date range is not valid, make sure the 'from' date is before the 'until' date."
 };
 
+/**
+ * Error message when the "until" date is set in the future.
+ */
+const futureUntilDateError = {
+    message: "Future dates are not allowed for the 'until' field."
+};
+
+/**
+ * Error message for forks count being below the minimum allowed threshold.
+ */
+const lessThanMinForksError = {
+    message: `Number of forks must be greater than ${MIN_QUERIABLE_FORKS}.`
+};
+
+/**
+ * Error message for forks count exceeding the maximum allowed threshold.
+ */
+const greaterThanMaxForksError = {
+    message: `Number of forks must be less than ${MAX_QUERIABLE_FORKS}.`
+};
+
+/**
+ * Error message for invalid repository input format.
+ * Expected format: <Owner>/<RepositoryName>
+ */
+const repositorySyntaxError = {
+    message: "Syntax must match <Owner>/<RepositoryName>."
+};
+
+/**
+ * Error message when the repository owner is invalid.
+ */
+const ownerError = {
+    message: "Invalid repository owner."
+};
+
+/**
+ * Error message when the repository name is invalid.
+ */
+const repositoryNameError = {
+    message: "Invalid repository name."
+};
+
+/**
+ * Error message for recently updated filter values outside the allowed range.
+ */
+const outOfRecentlyUpdatedRangeError = {
+    message: `Can only filter on recent updates between 
+              ${RECENT_ACTIVITY_MIN_MONTHS} months ago and ${RECENT_ACTIVITY_MAX_MONTHS} months ago.`
+};
+
+/**
+ * Errors related to the "From" date field in commit date range filters.
+ */
 const CommitsDateRangeFromInputErrors = {
     LaterFromDateError: laterFromDateError,
     UnknownError: unknownError
 } as const;
 
+/**
+ * Errors related to the "Until" date field in commit date range filters.
+ */
 const CommitsDateRangeUntilInputErrors = {
     LaterFromDateError: laterFromDateError,
     UnknownError: unknownError,
     FutureUntilDateError: futureUntilDateError
 } as const;
 
+/**
+ * Errors related to the fork count input field.
+ */
 const ForksCountInputErrors = {
     LessThanMinForksError: lessThanMinForksError,
     GreaterThanMaxForksError: greaterThanMaxForksError,
     UnknownError: unknownError
 } as const;
 
+/**
+ * Errors related to repository input validation.
+ */
 const RepositoryInputErrors = {
     RepositorySyntaxError: repositorySyntaxError,
     OwnerError: ownerError,
@@ -47,6 +104,9 @@ const RepositoryInputErrors = {
     UnknownError: unknownError
 } as const;
 
+/**
+ * Errors related to the "Recently Updated" filter.
+ */
 const RecentlyUpdatedInputErrors = {
     OutOfRecentlyUpdatedRangeError: outOfRecentlyUpdatedRangeError,
     UnknownError: unknownError
@@ -55,11 +115,18 @@ const RecentlyUpdatedInputErrors = {
 // Derive strict types from error objects
 type CommitsDateRangeFromInputErrorsType =
     typeof CommitsDateRangeFromInputErrors[keyof typeof CommitsDateRangeFromInputErrors];
+
 type CommitsDateRangeUntilInputErrorsType =
     typeof CommitsDateRangeUntilInputErrors[keyof typeof CommitsDateRangeUntilInputErrors];
-type ForksCountInputErrorsType = typeof ForksCountInputErrors[keyof typeof ForksCountInputErrors];
-type RepositoryInputErrorsType = typeof RepositoryInputErrors[keyof typeof RepositoryInputErrors];
-type RecentlyUpdatedInputErrorsType = typeof RecentlyUpdatedInputErrors[keyof typeof RecentlyUpdatedInputErrors];
+
+type ForksCountInputErrorsType =
+    typeof ForksCountInputErrors[keyof typeof ForksCountInputErrors];
+
+type RepositoryInputErrorsType =
+    typeof RepositoryInputErrors[keyof typeof RepositoryInputErrors];
+
+type RecentlyUpdatedInputErrorsType =
+    typeof RecentlyUpdatedInputErrors[keyof typeof RecentlyUpdatedInputErrors];
 
 export {
     CommitsDateRangeFromInputErrors,

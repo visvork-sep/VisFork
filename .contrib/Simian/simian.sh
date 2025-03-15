66c4ba6501026710d4abdc6db3961b5a88118b1d
Jason Fu <r.fu1@student.tue.nl>
2025-03-15 23:32:03 +0000

    fix include and exclude paths

--- .contrib/Simian/simian.sh
+++ .contrib/Simian/simian.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar simian-4.0.0.jar -includes=src/**.ts -excludes=src/**test.ts -excludes=test/**.ts -balanceParentheses+
+java -jar simian-4.0.0.jar -includes=src/**.ts|tsx -excludes=src/**test.ts|tsx -balanceParentheses+

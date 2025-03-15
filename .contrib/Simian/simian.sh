0e19f4712f0cdd4a47b0b85bb341a99d5b987119
Jason Fu <r.fu1@student.tue.nl>
2025-03-15 23:37:29 +0000

    escape pipe command

--- .contrib/Simian/simian.sh
+++ .contrib/Simian/simian.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar simian-4.0.0.jar -includes=src/**.ts|tsx -excludes=src/**test.ts|tsx -balanceParentheses+
+java -jar simian-4.0.0.jar -includes=src/**.ts\|tsx -excludes=src/**test.ts\|tsx -balanceParentheses+

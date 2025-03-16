184ddfe7d1bf376ed38104bfe144e12f99dfb24e
veszgergo <vesz.gergo@gmail.com>
2025-03-16 10:34:51 +0100

    Updated Simian to include .tsx files (while excluding their tests)

--- .contrib/Simian/simian.sh
+++ .contrib/Simian/simian.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar simian-4.0.0.jar -includes=src/**.ts -excludes=src/**test.ts -excludes=test/**.ts -balanceParentheses+
+java -jar simian-4.0.0.jar -includes=src/**.ts -includes=src/**.tsx -excludes=src/**test.ts -excludes=src/**test.tsx -excludes=test/**.ts -excludes=test/**.tsx -balanceParentheses+

a75d2abca014d9953a73e3541d94517d8e143804
veszgergo <vesz.gergo@gmail.com>
2025-03-16 22:39:46 +0100

    Modified ForkSortService according to tests

--- src/Filters/ForkSortService.ts
+++ src/Filters/ForkSortService.ts
@@ -3,7 +3,7 @@ import { ForkJSON } from "../Types/GithubTypes";
 
 export class ForkSortService {
     /**
-     * Sorts an array of forks based on the sorting criterion.
+     * Sorts an array of forks based on the sorting criterion in DESCENDING order.
      * 
      * @param forks the array of forks to be sorted.
      * @param by the sorting criterion to sort by.
@@ -14,32 +14,45 @@ export class ForkSortService {
      *         "newest", "latestCommit", "authorPopularity".
      */
     sortForks(forks: ForkJSON[], by: SortingCriterionExtra): ForkJSON[] {
-        return forks.sort((f1, f2) => {
+        // JavaScript's .sort() sorts in-place, so a shallow-copy is created
+        // to return a new, sorted array instead of just modifying the old one
+        // (better for testing)
+        return [...forks].sort((f1, f2) => {
+            let result = 0;
             switch (by) {
                 case "stargazers":
-                    return this.#compareNumberOfStargazers(f1, f2);
+                    result = this.#compareNumberOfStargazers(f1, f2);
+                    break;
                 case "watchers":
-                    return this.#compareNumberOfWatchers(f1, f2);
+                    result = this.#compareNumberOfWatchers(f1, f2);
+                    break;
                 case "oldest":
-                    return this.#compareAge(f1, f2);
+                    result = (-1) * this.#compareAge(f1, f2);
+                    break;
                 case "newest":
-                    return (-1) * this.#compareAge(f1, f2);
+                    result = this.#compareAge(f1, f2);
+                    break;
                 case "latestCommit":
-                    return this.#compareLatestCommits(f1, f2);
+                    result = this.#compareLatestCommits(f1, f2);
+                    break;
                 case "authorPopularity":
-                    return this.#compareAuthorPopularity(f1, f2);
+                    result = this.#compareAuthorPopularity(f1, f2);
+                    break;
                 default:
-                    throw Error("Unknown sorting criterion \"" + by + "\"");
+                    throw new Error(`Unknown sorting criterion "${by}"`);
             }
-        });
+        
+            // If comparison was equal (0), then sort them alphabetically
+            return result !== 0 ? result : f1.name.localeCompare(f2.name);
+        });    
     }
 
     /**
      * Compares the number of stargazers of {@param f1} to that of {@param f2}.
      * 
-     * @returns 0 if the number of stargazers in f1 and f2 are equal
-     *          -1 if the number of stargazers in f1 is less than that in f2
-     *          1 if the number of stargazers in f1 is more than that in f2
+     * @post    \result == 0 if the number of stargazers in f1 and f2 are equal
+     *          \result > 0 if the number of stargazers in f1 is less than that in f2
+     *          \result < 0 if the number of stargazers in f1 is more than that in f2
      * 
      * @throws TypeError if the stargazers_count of either f1 or f2 is null or undefined.
      */
@@ -48,16 +61,15 @@ export class ForkSortService {
             throw TypeError("Unable to read stargazers_count property of forks");
         }
 
-        return f1.stargazers_count === f2.stargazers_count ? 0 :
-               f1.stargazers_count > f2.stargazers_count ? 1 : -1;
+        return f2.stargazers_count - f1.stargazers_count;
     }
 
     /**
      * Compares the number of watchers of {@param f1} to that of {@param f2}.
      * 
-     * @returns 0 if the number of watchers in f1 and f2 are equal
-     *          -1 if the number of watchers in f1 is less than that in f2
-     *          1 if the number of watchers in f1 is more than that in f2
+     * @post    \result == 0 if the number of watchers in f1 and f2 are equal
+     *          \result > 0 if the number of watchers in f1 is less than that in f2
+     *          \result < 0 if the number of watchers in f1 is more than that in f2
      * 
      * @throws TypeError if the watchers_count property of either f1 or f2 is null or undefined.
      */
@@ -66,16 +78,15 @@ export class ForkSortService {
             throw TypeError("Unable to read watchers_count property of forks");
         }
 
-        return f1.watchers_count === f2.watchers_count ? 0 :
-               f2.watchers_count > f2.watchers_count ? 1 : -1;
+        return f2.watchers_count - f1.watchers_count;
     }
 
     /**
      * Compares the creation date of {@param f1} to that of {@param f2}.
      * 
-     * @returns 0 if f1 and f2 were created in the exact same millisecond
-     *          -1 if f1 was created before f2
-     *          1 if f1 was created after f2
+     * @post    \result == 0 if f1 and f2 were created in the exact same millisecond
+     *          \result > 0 if f1 was created before f2
+     *          \result < 0 if f1 was created after f2
      * 
      * @throws TypeError if the created_at property of either f1 or f2 is null or undefined.
      */
@@ -87,16 +98,15 @@ export class ForkSortService {
         const creationDateF1Milliseconds = Date.parse(f1.created_at);
         const creationDateF2Milliseconds = Date.parse(f2.created_at);
 
-        return creationDateF1Milliseconds === creationDateF2Milliseconds ? 0 :
-               creationDateF1Milliseconds > creationDateF2Milliseconds ? 1 : -1;
+        return creationDateF2Milliseconds - creationDateF1Milliseconds;
     }
 
     /**
      * Compares the date of the latest commit of {@param f1} to that of {@param f2}.
      * 
-     * @returns 0 if the date (minute/day/hour???) TODO of the latest commit of f1 and f2 are equal
-     *          -1 if the number of stargazers in f1 is less than that in f2
-     *          1 if the number of stargazers in f1 is more than that in f2
+     * @post    \result == 0 if the date of the latest commit of f1 and f2 are equal
+     *          \result > 0 if the number of stargazers in f1 is less than that in f2
+     *          \result < 0 if the number of stargazers in f1 is more than that in f2
      * 
      * @throws TypeError if the updated_at property of either f1 or f2 is null or undefined.
      */
@@ -108,8 +118,7 @@ export class ForkSortService {
         const lastCommitDateF1Milliseconds = Date.parse(f1.updated_at);
         const lastCommitDateF2Milliseconds = Date.parse(f2.updated_at);
 
-        return lastCommitDateF1Milliseconds === lastCommitDateF2Milliseconds ? 0 :
-               lastCommitDateF1Milliseconds > lastCommitDateF2Milliseconds ? 1 : -1;
+        return lastCommitDateF2Milliseconds - lastCommitDateF1Milliseconds;
     }
 
     /**

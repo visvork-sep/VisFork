529cc0c97fb5a9d90dc58038b09d4a5ede80fecf
NickFris <131258939+NickFris@users.noreply.github.com>
2025-03-18 00:12:18 +0100

    Histogram (#52)
    * Add D3.js
    
    * Copied over RangeSlider and example data, made RangeSlider render
    
    * Added border around RangeSlider, made naming consistent
    
    * Rewrote barchart code, barchart is now responsive to window size
    
    * X-axis label adaptive to window size, fixed visual bug of February and March bar having less spacing than other bars
    
    * Added more example data to test
    
    * fixed type
    
    * hovering over bars shows date and number of commits
    
    * added comments
    
    * moved hover logic to background bars for better functionality
    
    * selection window added, fixed space formatting
    
    * actually fixed spaces now
    
    * Finish merge changes.
    
    * small adjuststments
    
    * Small histogram with selection (it really is small, smol even)
    
    * Two charts one svg
    
    * refactored code
    
    * small adjustments
    
    * Almost working histogram!
    
    * Fix background bars of the focus chart.
    
    * Renamed from Test.tsx to Histogram.tsx
    
    * fixed import
    
    * added primer colors
    
    * added comments
    
    * added intial selection window on startup
    
    * changed general look, removed old histogram
    
    * dynamic x-axis labeling re-introduced
    
    * added interface for data for histogram
    
    * added range labels to focus chart
    
    * cleaned up code, edited comments
    
    * small adjustments and comments
    
    * removed unnecessary example data
    
    * removed old histogram file
    
    * small adjustments
    
    * file name
    
    * dont use example data file
    
    * fix caching issue ci
    
    * update package-lock
    
    * i dont fucking know man
    
    * dd
    
    ---------
    
    Co-authored-by: NickFris <n.frisen@student.tue.nl>
    Co-authored-by: Kala-Konarev <kkonarev144@gmail.com>
    Co-authored-by: Jason Fu <r.fu1@student.tue.nl>

--- src/Components/ApplicationBody.tsx
+++ src/Components/ApplicationBody.tsx
@@ -1,35 +1,15 @@
 import { Stack } from "@primer/react";
-
-import ForkList from "@Components/Plots/ForkList";
-import { Dropdown } from "@Components/Dropdown";
-
-const plotsData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+import Histogram from "./Plots/Histogram";
 
 function ApplicationBody() {
-    const children = plotsData.map((plot) => {
-        return (
-            <Stack.Item key={plot}>
-                <Dropdown summaryText="Fork List">
-                    <ForkList forks={[
-                        {
-                            id: 0,
-                            full_name: "name",
-                            description: "description"
-                        },
-                        {
-                            id: 1,
-                            full_name: "name2",
-                            description: "description 2"
-                        }
-                    ]} />
-                </Dropdown>
-            </Stack.Item>
-        );
-    });
-
+    const sampleCommitData = {
+        commits: [
+            { fork: "my-fork", date: new Date("2021-01-01") }
+        ],
+    };
     return (
         <Stack>
-            {children}
+            <Histogram commits={sampleCommitData.commits} />
         </Stack>
     );
 }
--- src/Components/Plots/Plot1.tsx
+++
@@ -1,9 +0,0 @@
-function Plot1() {
-    return (
-        <div>
-            <h1>Plot1</h1>
-        </div>
-    );
-}
-
-export default Plot1;

bd5e1046eea0a6a7c40a70a6de41c848a4a71145
aleks-chitalov <alex55bg@gmail.com>
2025-03-20 11:52:53 +0100

    First try for pagination

--- src/Queries/rawQueries.ts
+++ src/Queries/rawQueries.ts
@@ -1,9 +1,9 @@
 import { GetAvatarUrlDocument, GetAvatarUrlQueryVariables, GetForksDocument, GetForksQueryVariables }
     from "@generated/graphql";
-import { paths } from "@generated/rest-schema";
+import { paths, components } from "@generated/rest-schema";
 import request from "graphql-request";
 import createClient from "openapi-fetch";
-import { CommitQueryParams, ForkQueryParams, RepoQueryParams } from "../Types/GithubTypes";
+import { CommitQueryParams, ForkQueryParams, RepoQueryParams} from "../Types/GithubTypes";
 import { API_URL } from "@Utils/Constants";
 
 const GRAPHQL_URL = `${API_URL}/graphql`;
@@ -16,16 +16,31 @@ export async function fetchForksGql(parameters: GetForksQueryVariables, accessTo
     return request(GRAPHQL_URL, GetForksDocument, parameters, [["Authorization", "bearer" + accessToken]]);
 }
 
-/**
- * Fetch commits using REST API.
- */
-export async function fetchCommits(parameters: CommitQueryParams, accessToken: string) {
-    return fetchClient.GET("/repos/{owner}/{repo}/commits", {
-        params: parameters,
-        headers: {
-            Authorization: `Bearer ${accessToken}`,
-        },
-    });
+export async function fetchCommits(parameters: CommitQueryParams, accessToken: string, page = 1) {
+    const allCommits:  components["schemas"]["commit"][] = [];
+    let response;
+
+    do {
+        response = await fetchClient.GET("/repos/{owner}/{repo}/commits", {
+            params: { ...parameters, per_page: 100, page },
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+        });
+
+
+        if (response?.data) {
+            allCommits.push(...response.data); // Collect all commits in a single array:q
+        }
+        page++;
+
+    } while (response.response.headers.get("next")); // Continue until there is no next header
+
+    // Return a single FetchResponse-like object with all the data
+    return {
+        ...response, // Retain original FetchResponse structure
+        data: allCommits, // Replace data with the accumulated commits
+    };
 }
 
 /**

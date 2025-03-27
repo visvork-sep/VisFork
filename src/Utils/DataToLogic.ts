8f51354c305f8fff11f978b2cb9cdd3848255a2e
Jason Fu <95495174+fasonju@users.noreply.github.com>
2025-03-27 12:26:41 +0100

    git blame this (#88)

--- src/Utils/DataToLogic.ts
+++ src/Utils/DataToLogic.ts
@@ -21,7 +21,7 @@ export function toCommitInfo(commit: GitHubAPICommit): UnprocessedCommitExtended
         parentIds: commit.parents?.map(parent => parent.sha) ?? [],
         node_id: commit.node_id,
         author: commit.commit?.author?.name ?? "Unknown",
-        date: commit.commit?.author?.date ? new Date(commit.commit.author.date) : "Unknown",
+        date: commit.commit?.committer?.date ? new Date(commit.commit.committer.date) : "Unknown",
         url: commit.html_url,
         message: commit.commit?.message ?? "",
         branch: "",

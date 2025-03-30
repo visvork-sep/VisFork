6df1338417f02530d0446be1aa05437ab03e3844
Jason Fu <r.fu1@student.tue.nl>
2025-03-30 12:23:56 +0200

    make clear

--- src/Components/ConfigurationPane/FilterFormElements/ForksQueryOrderInput.tsx
+++ src/Components/ConfigurationPane/FilterFormElements/ForksQueryOrderInput.tsx
@@ -12,11 +12,15 @@ const options = Object.values(FORKS_SORTING_ORDERS).map((option) =>
     (<Select.Option value={option.value} key={option.value}>{option.label}</Select.Option>)
 );
 
+
+/**
+ * Select input for the metric to sort the forks by
+ */
 function ForksQueryOrderInput({ onChangeHandler, selected, error }: ForksQueryOrderInputProps) {
     return (
         <FormControl>
-            <FormControl.Label>Select forks by</FormControl.Label>
-            <FormControl.Caption>The metric to select forks by</FormControl.Caption>
+            <FormControl.Label>Fork selection option metric</FormControl.Label>
+            <FormControl.Caption>Metric to select the forks by in descending order</FormControl.Caption>
             <Select onChange={e => onChangeHandler(e.target.value)} value={selected}>
                 {options}
             </Select>

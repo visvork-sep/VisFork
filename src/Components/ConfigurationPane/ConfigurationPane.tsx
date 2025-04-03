2dbc5fe305eb7744e7bb86c3d11f25e79d727272
Jason Fu <95495174+fasonju@users.noreply.github.com>
2025-04-03 14:55:27 +0200

    update (#129)

--- src/Components/ConfigurationPane/ConfigurationPane.tsx
+++ src/Components/ConfigurationPane/ConfigurationPane.tsx
@@ -1,10 +1,8 @@
 import { Stack } from "@primer/react";
-import { Pagehead } from "@primer/react/deprecated";
 import FilterForm from "@Components/ConfigurationPane/FilterForm";
-import FileButtons from "@Components/ConfigurationPane/FileButtons";
 import { FilterChangeHandler } from "@Hooks/useFilteredData";
 
-interface ConfigurationPaneProps { 
+interface ConfigurationPaneProps {
     filterChangeHandler: FilterChangeHandler;
 };
 
@@ -12,12 +10,7 @@ function ConfigurationPane({ filterChangeHandler }: ConfigurationPaneProps) {
     return (
         <Stack gap="condensed">
             <Stack.Item>
-                <FilterForm filterChangeHandler={filterChangeHandler}/>
-            </Stack.Item>
-
-            <Pagehead>Import/Export</Pagehead>
-            <Stack.Item>
-                <FileButtons />
+                <FilterForm filterChangeHandler={filterChangeHandler} />
             </Stack.Item>
         </Stack>
     );

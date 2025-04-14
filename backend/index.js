4f961354648989103e3d2fdff5705de86d790058
SUDO-Gabor <g.viniczay@student.tue.nl>
2025-04-14 13:17:10 +0200

    Linter adjustment

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-require-imports */
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
